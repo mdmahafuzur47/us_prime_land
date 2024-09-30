cb4072691e11002d9cb2f95dd9264ebf1121e8aa
mdmahafuzur47 <mdmahafuzur4747@gmail.com>
2024-09-30 18:15:11 +0600

    create 1 file and update 2 files

--- src/pages/admin/propertyManagement/index.tsx
+++ src/pages/admin/propertyManagement/index.tsx
@@ -1,5 +1,22 @@
-import React from "react";
+import AddProperty from "@/components/pages/admin/property/AddProperty";
+import { Button } from "antd";
+import React, { useState } from "react";
 
 export default function PropertyManagement() {
-  return <div>Property Management</div>;
+  const [add,setAdd] = useState(false);
+  const toggleAdd = () => {
+    setAdd((prev) => !prev);
+  }
+  return <div>
+    <div>
+      <Button className="font-medium" onClick={toggleAdd}>Add</Button>
+    </div>
+
+    <div className="mt-5">
+      {/* Add property form  */}
+      {
+        add && <AddProperty />
+      }
+    </div>
+  </div>;
 }
